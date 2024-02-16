645207295a422dcbe9aaf68cbac61301363b2f91
helen katsimicha <helenkatsimiha@gmail.com>
2024-02-16 15:26:46 +0200

    Refactored plot_variable_pairs function to handle multiple variables

--- dataVisualization_2.py
+++ dataVisualization_2.py
@@ -8,39 +8,26 @@ df = pd.read_csv('housing_filled.csv')
 #setting the style for the plot
 sns.set_theme(style='whitegrid')
 
-#creating the scatter plot using seaborn
-#in this instance of the plot each dot corresponds to a single combination of 2 variables:
-#on the x axis, the amount of total rooms, and on the y axis the amount of total bedrooms
-sns.scatterplot(data=df, x='total_rooms', y='total_bedrooms', alpha=0.3)
-
-#showing the plot
-plt.show()
-
-# #!TODO make this work for every pair 3 of variables, this is a test
-# vars_to_plot = ['total_rooms', 'total_bedrooms', 'median_income']
-
-# #plotting the scatter matrix between 3 variables
-# sns.pairplot(df[vars_to_plot], kind='scatter')
-# plt.show()
-
-# #!TODO make this work for every pair of 4 variables
-# vars_to_plot = ['total_rooms', 'total_bedrooms', 'median_income', 'median_house_value']
-
-# #plotting the scatter matrix between 4 variables
-# sns.pairplot(df[vars_to_plot])
-# plt.show()
-
-#!TODO: clean up code, write comments for the function 
 def plot_variable_pairs(data, variables):
-    print(len(variables))
-    if len(variables) > 2:
-        pairs = sns.pairplot(data[variables], kind='scatter')
-        pairs.figure.suptitle(f"Scatter Matrix Plots for {', '.join(variables)}")
+    """This function generates a scatter plot for a pair of 2 variables, or 
+        a scatter matrix for pairs of multiple variables
 
-    else:
-        sns.scatterplot(data, x=variables[0], y=variables[1], alpha=0.3)
+    Args:
+        data (pandas.DataFrame): Our dataset containing the variables' values
+        variables (list): A list of the variables we want to plot
+    
+    """
+    
+    #if we have a set of 2 variables, we will use the seaborn.scatterplot() function 
+    #*NOTE:the alpha parameter controls the transparency of the points
+    if len(variables) == 2:
+        sns.scatterplot(data, x=variables[0], y=variables[1], alpha=0.3) 
         plt.title(f"Scatter Plot for {variables[0]} and {variables[1]}")
         
+    #if we have more than 2 variables, we will create a scatter matrix using the seaborn.pairplot() function
+    elif len(variables) > 2:
+        pairs = sns.pairplot(data[variables], kind='scatter')
+        pairs.figure.suptitle(f"Scatter Matrix Plots for {', '.join(variables)}")
     plt.show()
 
 
