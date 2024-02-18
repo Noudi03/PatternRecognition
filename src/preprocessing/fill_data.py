a65fd51c6c8d03013f9da6b34b7efada759f5459
helen katsimicha <helenkatsimiha@gmail.com>
2024-02-19 01:37:58 +0200

    fixed BUG

--- src/preprocessing/fill_data.py
+++ src/preprocessing/fill_data.py
@@ -30,24 +30,26 @@ def check_empty_fields(path):
         csv_file_path_filled = construct_absolute_path('housing_filled.csv')
         
         #filling the missing values with the median of the column
-        fill_empty_fields(numerical_df, csv_file_path_filled)
+        fill_empty_fields(df, csv_file_path_filled)
         
     else:
         print("No empty fields found in the CSV file.")
 
-def fill_empty_fields(numerical_df, path):
-    """Filling any empty numerical fields in the dataset with the median of the column
+def fill_empty_fields(df, path):
+    """Filling filled_df, 'filled_df, 'filled_df, 'filledpty numerical fields in the dataset with the median of the column
 
     Args:
-        numerical_df (pd.DataFrame): The dataset to be filled.
+        df (pd.DataFrame): The dataset to be filled.
+        numerical_df (pd.Dataframe): The numerical fields of the dataframe
         path (str): Path to the CSV file where the filled dataset will be saved
     Returns:
         None
     """
     #creating a copy of the dataset
-    df_filled = numerical_df.copy()
+    df_filled = df.copy()
+    numerical_df =  df_filled[get_numerical_features(df)]
     
-    #filling the empty fields of the dataset with the median of taht fields column
+    #filling the empty fields of the dataset with the median of that fields column
     df_filled.fillna(numerical_df.median(), inplace=True)
     
     #saving the filled dataset to a new CSV file
