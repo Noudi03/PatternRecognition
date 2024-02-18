d692d7a553f7356afb3f09ba9aab50d09c14bdd6
helen katsimicha <helenkatsimiha@gmail.com>
2024-02-18 18:35:59 +0200

    refactoring code

--- src/preprocessing/data_preprocessing_2.py
+++ src/preprocessing/data_preprocessing_2.py
@@ -1,27 +1,52 @@
-import pandas as pd
 from scipy.stats import zscore
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from utils import construct_absolute_path
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+from ..utils.utils import load_dataset,  get_numerical_features
 
-#loading the housing dataset
-csv_file_path = construct_absolute_path('housing.csv')
-df = pd.read_csv(csv_file_path)
+#loading the housing dataset with missing values filled
+df = load_dataset('housing_filled.csv')
 
-#selecting the numerical features only (could also return the values from the previous snippet might fix it)
-numerical_df = df[['longitude', 'latitude', 'housing_median_age', 'total_rooms', 'total_bedrooms', 'population', 'households', 'median_income', 'median_house_value']]
+#!using 4 different scalers to compare the results
 
-#!using 3 different scalers to compare the results
+def scale_data(df, scaler, scaler_name, check_std=True):
+    """Scaling the numerical features of a dataset using the specified scaler
+        Types supported: StandardScaler, MinMaxScaler, RobustScaler, zscore Scaler
 
-#applying the zscore scaler
-scaled_df_zscore = numerical_df.apply(zscore)
-print(scaled_df_zscore)
+    Args:
+        df (pd.DataFrame): The dataset to be scaled
+        scaler (sklearn.preprocessing._data.StandardScaler,
+                sklearn.preprocessing._data.MinMaxScaler, 
+                sklearn.preprocessing._data.RobustScaler
+                scipy.stats.zscore Scaler): The scaler to be used
+        check_std (bool, optional): Whether to check the standard deviation of the columns. Defaults to True.
+    Returns:
+        pd.DataFrame: The scaled dataset
+        check_std (bool, optional): Whether to check the standard deviation of the columns. Defaults to True.
+    """
+    
+    #selecting the numerical features only
+    numerical_df = df[get_numerical_features(df)]
+    
+    #checking to see what the standard deviation of each column is
+    #there could be a possible division by 0 if the standard deviation is 0
+    #in this case this isn't a problem but i'm leaving this here anyway
+    if not check_std:
+        for column in numerical_df.columns:
+            column_std = numerical_df[column].std()
+            print(f"The standard deviation of {column} is: {column_std}")
+            if column_std == 0:
+                print(f"The standard deviation of {column} is 0, so it is not possible to apply the scaler to this column")
+                numerical_df = numerical_df.drop(column, axis=1)
 
-#initialization of the standard scaler, fitting and transforming the data
-scaler = StandardScaler()
-scaled_df_standard = scaler.fit_transform(numerical_df)
-print(scaled_df_standard)
+    if scaler_name == "zscore":
+        scaled_df = numerical_df.apply(zscore)
+    else:
+        scaled_df = scaler.fit_transform(numerical_df)
+    
+    print(f"\nThe dataset has been scaled using {scaler_name}:\n")
+    print(scaled_df)
+    return scaled_df
 
-#initialization of the MinMax scaler, fitting and transforming the data
-scaler = MinMaxScaler()
-scaled_df_MinMax = scaler.fit_transform(numerical_df)
-print(scaled_df_MinMax)
+scaled_df_zscore = scale_data(df, zscore, "zscore", check_std=False)
+scaled_df_standard = scale_data(df, StandardScaler(), "StandardScaler")
+scaled_df_MinMax = scale_data(df, MinMaxScaler(), "MinMaxScaler")
+scaled_df_Robust = scale_data(df, RobustScaler(), "RobustScaler")
