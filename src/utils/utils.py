26873969039959ff8c26c1dfa78731741d7b1b1a
helen katsimicha <helenkatsimiha@gmail.com>
2024-02-18 19:16:02 +0200

    updated utils.py with more common functions (might change it later idk)

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -22,3 +22,25 @@ def load_dataset(file_name):
     """
     file_path = construct_absolute_path(file_name)
     return pandas.read_csv(file_path)
+
+def get_numerical_features(df):
+    """Identifying the numerical features in a dataset
+
+    Args:
+        df (pd.DataFrame): The dataset to be analyzed
+    Returns:
+        list: The list of numerical features
+    """
+    numerical_features = df.select_dtypes(include=['float64', 'int64']).columns.to_list()
+    return numerical_features
+
+def get_categorical_features(df):
+    """Identifying the categorical features in a dataset
+
+    Args:
+        df (pd.DataFrame): The dataset to be analyzed
+    Returns:
+        list: The list of categorical features
+    """
+    categorical_features = df.select_dtypes(include=['object']).columns.to_list()
+    return categorical_features
