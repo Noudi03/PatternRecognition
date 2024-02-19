5ccd7ef0fe109c186166e09ae028c27730bf2648
helen katsimicha <helenkatsimiha@gmail.com>
2024-02-19 15:45:42 +0200

    changes to the main.py file

--- src/main.py
+++ src/main.py
@@ -6,42 +6,48 @@ from preprocessing.one_hot_encode import one_hot_encode_data
 from preprocessing.fill_data import check_empty_fields
 from visualization.plot_histograms import plot_histogram, plot_categorical
 from visualization.plot_pairs import plot_variable_pairs
-#loading the housing dataset
-df = load_dataset('housing.csv')
 
-#calling the functions to get the numerical and categorical features
-numerical_features = get_numerical_features(df)
-categorical_features = get_categorical_features(df)
+def main():
+    #loading the housing dataset
+    df = load_dataset('housing.csv')
 
-#displaying the results
-print("The numerical features are : ", numerical_features)
-print("The categorical_features are:", categorical_features)
+    #calling the functions to get the numerical and categorical features
+    numerical_features = get_numerical_features(df)
+    categorical_features = get_categorical_features(df)
 
-scaled_df_standard = scale_data(df, StandardScaler(), "StandardScaler", check_std=False)
-scaled_df_MinMax = scale_data(df, MinMaxScaler(), "MinMaxScaler")
-scaled_df_Robust = scale_data(df, RobustScaler(), "RobustScaler")
+    #displaying the results
+    print("The numerical features are : ", numerical_features)
+    print("The categorical_features are:", categorical_features)
 
-#applying the one-hot encoding to the dataset
-ohe_df = one_hot_encode_data(df)
+    #!TODO Will probably use this for the regression algorithms idk yet no need to store all of them in variables
+    scaled_df_standard = scale_data(df, StandardScaler(), "StandardScaler", check_std=False) 
+    scaled_df_MinMax = scale_data(df, MinMaxScaler(), "MinMaxScaler")
+    scaled_df_Robust = scale_data(df, RobustScaler(), "RobustScaler")
 
-#printing the data
-print(ohe_df)
+    #applying the one-hot encoding to the dataset
+    ohe_df = one_hot_encode_data(df)
 
-#checking for empty fields in housing.csv
-csv_file_path = construct_absolute_path('housing.csv')
-check_empty_fields(csv_file_path)
+    #printing the data
+    print(ohe_df)
 
-#checking for empty fields in housing_filled.csv
-csv_file_path_filled = construct_absolute_path('housing_filled.csv')
-check_empty_fields(csv_file_path_filled)
+    #checking for empty fields in housing.csv
+    csv_file_path = construct_absolute_path('housing.csv')
+    check_empty_fields(csv_file_path)
 
-filled_df = load_dataset('housing_filled.csv')
-plot_categorical(filled_df)
-plot_histogram(filled_df)
+    #checking for empty fields in housing_filled.csv
+    csv_file_path_filled = construct_absolute_path('housing_filled.csv')
+    check_empty_fields(csv_file_path_filled)
 
-vars_to_plot_2 = ['total_rooms', 'total_bedrooms']
-plot_variable_pairs(df, vars_to_plot_2)
-vars_to_plot_3 = ['total_rooms', 'total_bedrooms', 'median_income']
-plot_variable_pairs(df, vars_to_plot_3)
-vars_to_plot_4 = ['total_rooms', 'total_bedrooms', 'median_income', 'median_house_value']
-plot_variable_pairs(df, vars_to_plot_4)
+    filled_df = load_dataset('housing_filled.csv')
+    plot_categorical(filled_df)
+    plot_histogram(filled_df)
+
+    vars_to_plot_2 = ['total_rooms', 'total_bedrooms']
+    plot_variable_pairs(df, vars_to_plot_2)
+    vars_to_plot_3 = ['total_rooms', 'total_bedrooms', 'median_income']
+    plot_variable_pairs(df, vars_to_plot_3)
+    vars_to_plot_4 = ['total_rooms', 'total_bedrooms', 'median_income', 'median_house_value']
+    plot_variable_pairs(df, vars_to_plot_4)
+    
+if __name__ == "__main__":
+    main()
