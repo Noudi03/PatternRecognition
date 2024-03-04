c4d470d338d595234ebfcd978a194ca4d15d130d
Eleni Katsimicha <helenkatsimiha@gmail.com>
2024-03-04 20:04:09 +0200

    Update pyproject.toml and main.py

--- src/main.py
+++ src/main.py
@@ -11,14 +11,16 @@ def main():
     df = load_dataset('housing.csv')
 
     # calling the functions to get the numerical and categorical features
+    input("\nShow numerical and categorical features: ")
     numerical_features = get_numerical_features(df)
     categorical_features = get_categorical_features(df)
 
     # displaying the results
-    print("\nThe numerical features are : ", numerical_features)
-    print("The categorical_features are:", categorical_features)
+    print("\nThe numerical features are: ", numerical_features)
+    print("The categorical_features are: ", categorical_features)
 
     # checking for empty fields in housing.csv
+    input("\nCheck for empty fields: ")
     csv_file_path = construct_absolute_path('housing.csv')
     check_empty_fields(csv_file_path)
 
@@ -29,12 +31,16 @@ def main():
     filled_df = load_dataset('housing_filled.csv')
 
     # scaling the numerical features of the dataset
+    input("\nScale the dataset with Standard Scaler: \n")
     scaled_df_standard = scale_data(
         filled_df, StandardScaler(), "StandardScaler", check_std=False)
+    input("\nScale the dataset with MinMax Scaler: ")
     scale_data(filled_df, MinMaxScaler(), "MinMaxScaler")
+    input("\nScale the dataset with Robust Scaler: ")
     scale_data(filled_df, RobustScaler(), "RobustScaler")
 
     # applying the one-hot encoding to the dataset
+    input("\nOne hot encode the categorical features: ")
     ohe_df = ohe_data(filled_df)
     print(ohe_df)
 
@@ -43,21 +49,29 @@ def main():
     append_data(scaled_df_standard, ohe_df, csv_file_path)
 
     # visualization
+    input("\nPlot the categorical data: ")
     plot_categorical(filled_df)
+    input("\nPlot histograms: ")
     plot_histogram(filled_df)
 
+    input("\nPlot pairs for 2 variables: ")
     vars_to_plot_2 = ['total_rooms', 'total_bedrooms']
     plot_variable_pairs(df, vars_to_plot_2)
+    input("\nPlot pairs for 3 variables: ")
     vars_to_plot_3 = ['total_rooms', 'total_bedrooms', 'median_income']
     plot_variable_pairs(df, vars_to_plot_3)
+    input("\nPlot pairs for 4 variables: ")
     vars_to_plot_4 = ['total_rooms', 'total_bedrooms',
                       'median_income', 'median_house_value']
     plot_variable_pairs(df, vars_to_plot_4)
 
     # regression
     df = load_dataset('housing_final.csv')
+    input("\nRun perceptron: ")
     perceptron_algorithm(df, k=10, learning_rate=0.1)
+    input("\nRun least squares: ")
     least_squares_algorithm(df, num_folds=10)
+    input("\nRun mlp regression: ")
     mlp_regression(df)
 
 
